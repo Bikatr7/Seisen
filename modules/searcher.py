b0aecb6559006d9d2bc8f59830db33a3834e3b8d
Bikatr7 <Tetralon07@gmail.com>
2023-12-26 01:13:53 -0700

    made searcher into a static class

--- modules/searcher.py
+++ modules/searcher.py
@@ -1,17 +1,16 @@
-## built-in modules
-from __future__ import annotations ## used for cheating the circular import issue that occurs when i need to type check some things
-
+## built-in libraries
 import typing
 import msvcrt
 
 ## custom modules
-if(typing.TYPE_CHECKING): ## used for cheating the circular import issue that occurs when i need to type check some things
-    from handlers.local_handler import localHandler
+from modules.toolkit import Toolkit
+
+from handlers.local_handler import LocalHandler
 
 
 ##--------------------start-of-searcher------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-class searcher:
+class Searcher:
 
     '''
 
@@ -19,50 +18,31 @@ class searcher:
         
     '''
 
-##--------------------start-of-__init__()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    def __init__(self, local_handler:localHandler) -> None:
-
-        """
-        
-        Initializes a new searcher object.\n
-
-        Parameters:\n
-        local_handler (object - localHandler) : the local handler.\n
-        None.\n
-
-        Returns:\n
-        None.\n
-
-        """
-
-        self.local_handler = local_handler
-
 ##--------------------start-of-get_vocab_print_item_from_id()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def get_vocab_print_item_from_id(self, vocab_id:int) -> str:
+    @staticmethod
+    def get_vocab_print_item_from_id(vocab_id:int) -> str:
 
         """
         
-        Gets a print item for a vocab given an id.\n
+        Gets a print item for a vocab given an id.
 
-        Parameters:\n
-        self (object - searcher) : the searcher object.\n
-        vocab_id (int) : the id of the vocab we are getting a print item for.\n
+        Parameters:
+        vocab_id (int) : the id of the vocab we are getting a print item for.
 
-        Returns:\n
-        print_item (str) : the print item for the id.\n
+        Returns:
+        print_item (str) : the print item for the id.
         
         """
             
         target_vocab = None
 
-        for vocab in self.local_handler.vocab:
+        for vocab in LocalHandler.vocab:
             if(vocab.word_id == vocab_id):
                 target_vocab = vocab
 
         if(target_vocab == None):
-            raise self.IDNotFoundError(vocab_id)
+            raise Searcher.IDNotFoundError(vocab_id)
         
         mini_csep_print = [str(csep.csep_id) for csep in target_vocab.testing_material_answer_all]
 
@@ -83,17 +63,17 @@ class searcher:
     
 ##--------------------start-of-get_csep_print_item_from_id()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def get_csep_print_item_from_id(self, csep_id:int) -> str:
+    @staticmethod
+    def get_csep_print_item_from_id(csep_id:int) -> str:
 
         """
         
-        Gets a print item for a csep given a csep id.\n
+        Gets a print item for a csep given a csep id.
 
-        Parameters:\n
-        self (object - searcher) : the searcher object.\n
-        csep_id (int) : the id of the vocab we are getting a print item for.\n
+        Parameters:
+        csep_id (int) : the id of the vocab we are getting a print item for.
 
-        Returns:\n
+        Returns:
         print_item (str) : the print item for the id.\n
         
         """
@@ -101,14 +81,14 @@ class searcher:
         target_vocab = None
         target_csep = None
 
-        for vocab in self.local_handler.vocab:
+        for vocab in LocalHandler.vocab:
             for csep in vocab.testing_material_answer_all:
                 if(csep.csep_id == csep_id):
                     target_csep = csep
                     target_vocab = vocab
 
         if(target_csep == None or target_vocab == None):
-            raise self.IDNotFoundError(csep_id)
+            raise Searcher.IDNotFoundError(csep_id)
         
         print_item = (
             f"---------------------------------\n"
@@ -124,30 +104,30 @@ class searcher:
     
 ##--------------------start-of-get_csep_print_items_from_vocab_id()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def get_csep_print_items_from_vocab_id(self, vocab_id:int) -> typing.List[str]:
+    @staticmethod
+    def get_csep_print_items_from_vocab_id(vocab_id:int) -> typing.List[str]:
 
         """
         
-        Gets a print item for a csep given a vocab id.\n
+        Gets a print item for a csep given a vocab id.
 
-        Parameters:\n
-        self (object - searcher) : the searcher object.\n
-        vocab_id (int) : the id of the vocab we are getting a print item for.\n
+        Parameters:
+        vocab_id (int) : the id of the vocab we are getting a print item for.
 
-        Returns:\n
-        print_items (list - str) : the print item for the id.\n
+        Returns:
+        print_items (list - str) : the print item for the id.
         
         """
             
         target_vocab = None
         print_items = []
 
-        for vocab in self.local_handler.vocab:
+        for vocab in LocalHandler.vocab:
             if(vocab.word_id == vocab_id):
                 target_vocab = vocab
 
         if(target_vocab == None):
-            raise self.IDNotFoundError(vocab_id)
+            raise Searcher.IDNotFoundError(vocab_id)
         
         for csep in target_vocab.testing_material_answer_all:
 
@@ -166,24 +146,24 @@ class searcher:
 
 ##--------------------start-of-get_vocab_term_from_id()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def get_vocab_term_from_id(self, vocab_id:int) -> str:
+    @staticmethod
+    def get_vocab_term_from_id(vocab_id:int) -> str:
 
         """
         
-        Gets a vocab term given an id.\n
+        Gets a vocab term given an id.
 
-        Parameters:\n
-        self (object - searcher) : the searcher object.\n
-        vocab_id (int) : the id for the term we are searching for.\n
+        Parameters:
+        vocab_id (int) : the id for the term we are searching for.
 
-        Returns:\n
-        term (str) : the term if found, otherwise "-1".\n
+        Returns:
+        term (str) : the term if found, otherwise "-1".
 
         """
 
         term = "-1"
         
-        for vocab in self.local_handler.vocab:
+        for vocab in LocalHandler.vocab:
             if(vocab.word_id == vocab_id):
                 term = vocab.testing_material
 
@@ -191,19 +171,18 @@ class searcher:
     
 ##--------------------start-of-get_id_from_term()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def get_id_from_vocab_term(self, term:str) -> int:
+    @staticmethod
+    def get_id_from_vocab_term(term:str) -> int:
 
         """
         
-        Gets a vocab id given an verb.\n
+        Gets a vocab id given an verb.
 
-        Parameters:\n
-        self (object - searcher) : the searcher object.\n
-        tern (str) : the term for the id we are searching for.\n 
+        Parameters:
+        tern (str) : the term for the id we are searching for.
 
-        Returns:\n
-        final_id (int) : the id if found, otherwise -1.\n
-
+        Returns:
+        final_id (int) : the id if found, otherwise -1.
 
         """
 
@@ -213,7 +192,7 @@ class searcher:
 
         final_id = -1
 
-        for vocab in self.local_handler.vocab:
+        for vocab in LocalHandler.vocab:
             if(vocab.testing_material == term):
                 matching_ids.append(vocab.word_id)
 
@@ -224,11 +203,11 @@ class searcher:
             return final_id
         
         for id in matching_ids:
-            id_print_message += self.get_vocab_print_item_from_id(id)
+            id_print_message += Searcher.get_vocab_print_item_from_id(id)
 
         id_print_message += "\n\nWhich vocab are you looking for? (Enter position 1-" + str(len(matching_ids)) + ")"
 
-        target_index = int(self.local_handler.toolkit.input_check(4, str(msvcrt.getch().decode()), len(matching_ids), id_print_message)) - 1
+        target_index = int(Toolkit.input_check(4, str(msvcrt.getch().decode()), len(matching_ids), id_print_message)) - 1
 
         final_id = matching_ids[target_index]
 
@@ -236,24 +215,24 @@ class searcher:
     
 ##--------------------start-of-get_ids_from_japanese()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     
-    def get_ids_from_japanese(self, term:str) -> typing.List[int]:
+    @staticmethod
+    def get_ids_from_japanese(term:str) -> typing.List[int]:
     
         """
         
-        Gets vocab ids that match a given japanese term.\n
+        Gets vocab ids that match a given japanese term.
 
-        Parameters:\n
-        self (object - searcher) : the searcher object.\n
+        Parameters:
         term (str) : the term we are searching with.\n
 
-        Returns:\n
+        Returns:
         vocab_ids (list = int) : matching vocab ids.\n
 
         """
 
         vocab_ids = []
 
-        for vocab in self.local_handler.vocab:
+        for vocab in LocalHandler.vocab:
 
             if(term == vocab.testing_material or term == vocab.furigana):
                 vocab_ids.append(vocab.word_id)
@@ -262,26 +241,26 @@ class searcher:
 
 ##--------------------start-of-get_ids_from_alpha_term()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    def get_ids_from_alpha_term(self, term:str) -> typing.Tuple[typing.List[int], typing.List[int]]:
+    @staticmethod
+    def get_ids_from_alpha_term(term:str) -> typing.Tuple[typing.List[int], typing.List[int]]:
 
         """
         
-        Gets vocab and csep ids that match a given term.\n
+        Gets vocab and csep ids that match a given term.
 
-        Parameters:\n
-        self (object - searcher) : the searcher object.\n
-        term (str) : the term we are searching with.\n
+        Parameters:
+        term (str) : the term we are searching with.
 
-        Returns:\n
-        vocab_ids (list = int) : matching vocab ids.\n
-        csep_ids (list - int) : matching csep ids.\n
+        Returns:
+        vocab_ids (list = int) : matching vocab ids.
+        csep_ids (list - int) : matching csep ids.
 
         """
 
         vocab_ids = []
         csep_ids = []
 
-        for vocab in self.local_handler.vocab:
+        for vocab in LocalHandler.vocab:
 
             ## if term matches romaji or definition 
             if(vocab.romaji == term or vocab.testing_material_answer_main == term):
@@ -304,11 +283,10 @@ class searcher:
     class IDNotFoundError(Exception):
 
         """
-        
-        Is raised when an id is not found.\n
+    
+        Is raised when an id is not found.
 
         """
-
 
 ##--------------------start-of-__init__()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -317,13 +295,10 @@ class searcher:
 
             """
             
-            Initializes a new IDNotFoundError Exception.\n
+            Initializes a new IDNotFoundError Exception.
 
             Parameters:\n
-            id_value (int) : The id value that wasn't found.\n
-
-            Returns:\n
-            None.\n
+            id_value (int) : The id value that wasn't found.
 
             """
 

c4b543ccfe8eb7d91b63e243102eb7d184087f43
Bikatr7 <Tetralon07@gmail.com>
2023-12-26 23:03:56 -0700

    Refactor csep class initialization

--- entities/csep.py
+++ entities/csep.py
@@ -1,11 +1,11 @@
 
 class csep:
 
-    '''
+    """
 
-    The csep class is used to represent alternative answers to a word.\n
+    The csep class is used to represent alternative answers to a word.
         
-    '''
+    """
 
 ##--------------------start-of-__init__()------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -14,23 +14,20 @@ class csep:
 
         """
         
-        Initializes a new csep object.\n
+        Initializes a new csep object.
 
-        Parameters:\n
-        incoming_word_id (int) : The id of the word the csep is for.\n
-        incoming_csep_id (int) : The id of the csep.\n
-        incoming_csep_value (string) : The value of the csep.\n
-        incoming_word_type (string) : The type of the word the csep is for.\n
-        
-        Returns:\n
-        None.\n
-
+        Parameters:
+        incoming_word_id (int) : The id of the word the csep is for.
+        incoming_csep_id (int) : The id of the csep.
+        incoming_csep_value (str) : The value of the csep.
+        incoming_word_type (str) : The type of the word the csep is for.
+    
         """
 
-        self.word_id = incoming_word_id
+        self.word_id:int = incoming_word_id
 
-        self.csep_id  = incoming_csep_id
+        self.csep_id:int = incoming_csep_id
 
-        self.csep_value = incoming_csep_value
+        self.csep_value:str = incoming_csep_value
 
-        self.word_type = incoming_word_type
+        self.word_type:str = incoming_word_type
